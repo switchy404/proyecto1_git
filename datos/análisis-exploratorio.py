095faae6627bd636012d68089674645e40887901
Nicolás Yépez Obando <n.yepez@uniandes.edu.co>
2024-09-10 12:14:36 -0500

    correciones

--- datos/análisis-exploratorio.py
+++ datos/análisis-exploratorio.py
@@ -1,4 +1,15 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
 # Análisis Exploratorio de datos -------------------------------------------------------
+
+df = pd.read_csv("C:/Users/Nicolas/Downloads/SeoulBikeData_utf8.csv")
 
 df.head()
 variables = ['Hour', 'Temperature(C)', 'Rented Bike Count']
@@ -37,30 +48,27 @@ plt.show()
 # DeQu, este archivo ya sirve por completo :cat:
 
 # Calcular la correlación entre las variables numéricas
-correlation_matrix = data[['Rented_Bike_Count', 'Temperature', 'Hour', 'Season', 'Holiday']].corr()
+mcorr = df[['Rented Bike Count', 'Temperature(C)', 'Hour', 'Seasons', 'Holiday']].corr()
 
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+sns.heatmap(mcorr, annot=True, cmap='coolwarm')
 plt.title('Matriz de Correlación')
 plt.show()
 
 # Relación entre temperatura y bicicletas alquiladas
-sns.scatterplot(x='Temperature', y='Rented_Bike_Count', data=data)
+sns.scatterplot(x='Temperature(C)', y='Rented Bike Count', data=df)
 plt.title('Relación entre temperatura y bicicletas alquiladas')
 plt.show()
 
 # Relación entre hora del día y bicicletas alquiladas
-sns.scatterplot(x='Hour', y='Rented_Bike_Count', data=data)
+sns.scatterplot(x='Hour', y='Rented Bike Count', data=df)
 plt.title('Relación entre hora del día y bicicletas alquiladas')
 plt.show()
 
 # Pairplot para ver relaciones entre múltiples variables
-sns.pairplot(data[['Rented_Bike_Count', 'Temperature', 'Hour', 'Season', 'Holiday']])
+sns.pairplot(df[['Rented Bike Count', 'Temperature(C)', 'Hour', 'Seasons', 'Holiday']])
 plt.show()
 
-# Matriz de correlación para todas las variables numéricas
-corr_matrix = data[['Rented_Bike_Count', 'Temperature', 'Hour', 'Season', 'Holiday']].corr()
-
 # Heatmap de la matriz de correlación
-sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+sns.heatmap(mcorr, annot=True, cmap='coolwarm')
 plt.title('Matriz de Correlación entre Variables')
 plt.show()
